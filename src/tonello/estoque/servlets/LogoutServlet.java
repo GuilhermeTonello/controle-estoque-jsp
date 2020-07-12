af49b528dae2e9ad229efb0d1360526fedf44c20
Guilherme <guitonello@outlook.com.br>
2020-07-12 11:45:07 -0300

    Logout feito

---
+++ src/tonello/estoque/servlets/LogoutServlet.java
@@ -0,0 +1,31 @@
+package tonello.estoque.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public LogoutServlet() {
+		super();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getSession().removeAttribute("logado");
+		req.getRequestDispatcher("index.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+	
+}
