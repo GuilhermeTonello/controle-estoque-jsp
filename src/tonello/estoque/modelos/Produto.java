7a0e39b5351191c9aeb4692d2cc1a91946ec7f1b
Guilherme <guitonello@outlook.com.br>
2020-07-12 11:40:08 -0300

    Modelo Produto adicionado

---
+++ src/tonello/estoque/modelos/Produto.java
@@ -0,0 +1,51 @@
+package tonello.estoque.modelos;
+
+public class Produto {
+	
+	private long id;
+	private String nome;
+	private String descricao;
+	private int quantidade;
+	private double valor;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+}
