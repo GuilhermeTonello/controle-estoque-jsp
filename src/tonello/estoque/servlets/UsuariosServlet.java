ca0a4db14bbe534b9700be087e3c3c579d4f4586
Guilherme <guitonello@outlook.com.br>
2020-07-14 09:02:38 -0300

    Deletar, editar e criar novo usuário

--- src/tonello/estoque/servlets/UsuariosServlet.java
+++ src/tonello/estoque/servlets/UsuariosServlet.java
@@ -25,7 +25,38 @@ public class UsuariosServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("adicionando/editando usuário");
+		String id = request.getParameter("id");
+		String nome = request.getParameter("nome");
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		Usuario usuario = new Usuario();
+		if (id != null && !id.isEmpty())
+			usuario.setId(Long.parseLong(id));
+		usuario.setNome(nome);
+		usuario.setLogin(login);
+		usuario.setSenha(senha);
+		
+		if (nome == null || nome.isEmpty() 
+				|| login == null || login.isEmpty()
+				|| senha == null || senha.isEmpty()) {
+			voltarFormUsuario(request, response, "null-input");
+		} else {
+			if (usuarioDao.validar(usuario.getLogin())) {
+				String mensagem;
+				if (id == null || id.isEmpty()) {
+					usuarioDao.adicionar(usuario);
+					mensagem = "Usuário " + usuario.getNome() + " adicionado com sucesso.";
+				} else {
+					usuarioDao.atualizar(usuario);
+					mensagem = "Usuário " + usuario.getNome() + " editado com sucesso.";
+				}
+				request.setAttribute("sucesso", mensagem);
+				listar(request, response);
+			} else {
+				voltarFormUsuario(request, response, "ja-existe");
+			}
+		}
 	}
 
 	@Override
@@ -36,14 +67,22 @@ public class UsuariosServlet extends HttpServlet {
 		if (acao != null && !acao.isEmpty() && acao.equals("adicionar")) {
 			request.getRequestDispatcher("sistemas/usuarios-pages/form-usuario.jsp").forward(request, response);
 		} else if (acao != null && !acao.isEmpty() && id != null && !id.isEmpty() && acao.equals("deletar")) {
-			System.out.println("deletar");
+			Usuario usuarioEscolhido = usuarioDao.consultarPorId(Long.parseLong(id));
+			if (usuarioEscolhido != null) {
+				request.setAttribute("sucesso", "Usuário " + usuarioEscolhido.getNome() + " deletado com sucesso");
+				usuarioDao.deletar(Long.parseLong(id));
+				listar(request, response);
+			} else {
+				request.setAttribute("erro", "Esse usuário não existe.");
+				listar(request, response);
+			}
 		} else if (acao != null && !acao.isEmpty() && id != null && !id.isEmpty() && acao.equals("editar")) {
 			Usuario usuarioEscolhido = usuarioDao.consultarPorId(Long.parseLong(id));
 			if (usuarioEscolhido != null) {
 				request.setAttribute("usuario", usuarioEscolhido);
 				request.getRequestDispatcher("sistemas/usuarios-pages/form-usuario.jsp").forward(request, response);
 			} else {
-				request.setAttribute("erro", "Esse produto não existe.");
+				request.setAttribute("erro", "Esse usuário não existe.");
 				listar(request, response);
 			}
 		} else {
@@ -54,6 +93,14 @@ public class UsuariosServlet extends HttpServlet {
 	private void listar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute("usuarios", usuarioDao.listar());
 		request.getRequestDispatcher("sistemas/usuarios-pages/usuarios-index.jsp").forward(request, response);
+	}
+	
+	private void voltarFormUsuario(HttpServletRequest request, HttpServletResponse response, String erro) throws ServletException, IOException {
+		if (request.getParameter("id") == null || request.getParameter("id").isEmpty()) {
+			response.sendRedirect("usuarios?acao=adicionar&erro=" + erro);
+		} else {
+			response.sendRedirect("usuarios?acao=editar&id=" + request.getParameter("id") + "&erro=" + erro);
+		}
 	}
 
 }
